76508268f889554d7bdb7f05e7a1a714c031dbe0
Max <79370059+Max835345@users.noreply.github.com>
2024-09-16 16:36:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ while run:
             if i.button == 1:
                 pygame.draw.circle(root, (225, 0, 50), i.pos, 20)
                 pygame.display.update()
+            if i.button == 2:
+                root.fill((0, 0, 0))
+                pygame.display.update()
             if i.button == 3:
                 pygame.draw.circle(root, (0, 0, 225), i.pos, 20)
                 pygame.draw.rect(root, (0, 225, 0), (i.pos[0] - 10, i.pos[1] - 10, 20, 20))
                 pygame.display.update()
-
 
 pygame.quit()
